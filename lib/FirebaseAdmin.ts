147081b5c913c8e4ae1694d8421e053af9bcd85a
siddharthdangi1911 <siddharthdangi1911@gmail.com>
2025-12-07 22:27:48 +0530

    database error resolved

--- lib/FirebaseAdmin.ts
+++ lib/FirebaseAdmin.ts
@@ -6,8 +6,8 @@ if (!admin.apps.length) {
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-      
     }),
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
   });
 }
 
