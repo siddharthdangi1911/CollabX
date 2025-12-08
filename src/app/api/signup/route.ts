944798fe5814b0f797750a00e9da6d7f19eec57b
siddharthdangi1911 <siddharthdangi1911@gmail.com>
2025-12-08 16:34:35 +0530

    commit

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -20,6 +20,6 @@ export async function POST(req: Request) {
 
     return NextResponse.json({ success: true });
   } catch (error: any) {
-    return NextResponse.json({ success: false, error: error.message }, { status: 400 });
+    return NextResponse.json({ success: false, error: error}, { status: 400 });
   }
 }
